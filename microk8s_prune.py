fa253280959012086813b58bd5fe9995231f95cc
shinodark <shinooonline@gmail.com>
2023-01-03 10:29:27 +0100

    Update microk8s_prune.py
    Use argparse

--- microk8s_prune.py
+++ microk8s_prune.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
 from datetime import datetime
 
 import grpc
 from containerd.services.containers.v1 import containers_pb2_grpc, containers_pb2
 from containerd.services.images.v1 import images_pb2_grpc, images_pb2
 from containerd.services.content.v1 import content_pb2_grpc, content_pb2
-
-# args :   c print containers and associated images
-#          i print images name
-#          u print unused images name
-#          s print sumary info
-#          p DELETE unused images (if run interactively, will request confirmation)
-#          f force delete without confirmation
-
 
 def compute_size(contentv1, imgDigest, doneLayer=None):
     content = contentv1.Info( content_pb2.InfoRequest(digest=imgDigest),
@@ -35,7 +28,6 @@ def compute_size(contentv1, imgDigest, doneLayer=None):
             pass # Layer not found in content ?
     return size
 
-
 # From Fred Cirera on StackOverflow : thanks !
 # https://stackoverflow.com/questions/1094841/get-human-readable-version-of-file-size
 def sizeof_fmt(num, suffix='B'):
@@ -45,9 +37,25 @@ def sizeof_fmt(num, suffix='B'):
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
+parser = argparse.ArgumentParser(prog="Prune image for microk8s")
+parser.add_argument("-c", "--listcontainers", action="store_true",
+    help="print containers and associated images")
+parser.add_argument("-i", "--listimages", action="store_true",
+    help="print images name")
+parser.add_argument("-u", "--listunused", action="store_true",
+    help="print unused images name")
+parser.add_argument("-s", "--info", action="store_true",
+    help="print sumary info")
+parser.add_argument("-p", "--prune", action="store_true",
+    help="DELETE unused images (if run interactively, will request confirmation)")
+parser.add_argument("-f", "--force", action="store_true",
+    help="force delete without confirmation")
+args = parser.parse_args()
+if not any(vars(args).values()):
+    parser.print_help()
+    sys.exit()
 
-args = ",".join(sys.argv[1:]).lower()
-if "p" in args and not "f" in args and sys.stdout.isatty():
+if args.prune and not args.force and sys.stdout.isatty():
     resp = input("Unused images will be deleted, please confirm [y/N]: ")
     if resp.upper() != "Y":
         print("Operation cancelled")
@@ -67,7 +75,7 @@ with grpc.insecure_channel('unix:///var/snap/microk8s/common/run/containerd.sock
     usedImages = {}
     for c in containers:
         usedImages[c.image] = c.id
-        if "c" in args: print("C:", c.id, c.image)
+        if args.listcontainers: print("C:", c.id, c.image)
 
     images = imagesv1.List( images_pb2.ListImagesRequest(),
                             metadata=(('containerd-namespace', 'k8s.io'),)).images
@@ -81,19 +89,19 @@ with grpc.insecure_channel('unix:///var/snap/microk8s/common/run/containerd.sock
         imageSize = compute_size(contentv1, i.target.digest)
         totalImageSize += imageSize
         netTotalSize += compute_size(contentv1, i.target.digest, doneLayer)
-        if "i" in args:
+        if args.listimages:
             print("I:", i.name,
                         imageSize,
                         datetime.fromtimestamp(i.updated_at.seconds).isoformat())
 
-    if "u" in args:
+    if args.listunused:
         for i in unused: print("U:", i)
 
-    if "p" in args:
+    if args.prune:
         for i in unused: imagesv1.Delete( images_pb2.DeleteImageRequest( name=i, sync=True),
                                           metadata=(('containerd-namespace', 'k8s.io'),) )
 
-    if "s" in args:
+    if args.info:
 
         print("S:", len(containers), "containers")
         print("S:", len(images), "total images")
@@ -101,7 +109,7 @@ with grpc.insecure_channel('unix:///var/snap/microk8s/common/run/containerd.sock
         print("S:", len(usedImages), "used images")
         if unused: print("S:", len(unused), "unused images")
 
-        if "p" in args:
+        if args.prune:
             images = imagesv1.List( images_pb2.ListImagesRequest(),
                                     metadata=(('containerd-namespace', 'k8s.io'),)).images
             newNetImageSize = 0
